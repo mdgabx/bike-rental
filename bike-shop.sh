0c30aee2e375897819497beaa86bf0c5f8e53a6a
Mark Dexter <a.markdexter@gmail.com>
2023-03-20 12:05:45 +0800

    Create bike-shop.sh

---
+++ bike-shop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo -e "\n~~~~~ Bike Rental Shop ~~~~~\n"
+
+MAIN_MENU() {
+  echo "How may I help you?"
+  echo -e "\n1. Rent a bike\n2. Return a bike\n3. Exit"
+
+  read MAIN_MENU_SELECTION
+}
+
+RENT_MENU() {
+  echo "Rent Menu"
+}
+
+MAIN_MENU
